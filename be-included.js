28b38da6a03a19188f407d095d94327ec6f19652
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-06-07 08:53:17 -0400

    Initial commit

---
+++ be-included.js
@@ -0,0 +1,4 @@
+// @ts-check
+import { propInfo, rejected, resolved } from 'be-enhanced/cc.js';
+import { BE } from 'be-enhanced/BE.js';
+import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
